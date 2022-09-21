83f0991dfa0cb565b838cefedc0e6d20fd6f1b9b
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-20 20:06:02 -0400

    server/utils/auth.js created and added back

---
+++ server/utils/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+    signToken: function ({ username, email, _id }) {
+        const payload = { usernmae, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+
+    // function for our authenticated routes
+    authMiddleware: function (req, res, next) {
+        // allows token to be sent via req.body, req.query, or headers
+        let token =
+            req.body.token || req.query.token || req.headers.authorization;
+
+        // ["Bearer", "<tokenvalue>"]
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        // If no token exists return the request object as it is
+        if (!token) {
+            return req;
+        }
+
+        // verify token and get user data out of it
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+            return res.status(400).json({ message: 'invalid token!' });
+        }
+
+        // returning up to date request object
+        return req;
+    },
+};
