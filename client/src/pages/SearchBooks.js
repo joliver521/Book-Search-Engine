a6f2abd2812b36e55267a262af9e8881d46dabd1
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-21 22:07:59 -0400

    SavedBooks.js and SearchBooks.js updated

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,17 +10,23 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
 
 const SearchBooks = () => {
     // create state for holding returned google api data
     const [searchedBooks, setSearchedBooks] = useState([]);
+
     // create state for holding our search field data
     const [searchInput, setSearchInput] = useState('');
 
     // create state to hold saved bookId values
     const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+    const [saveBook] = useMutation(SAVE_BOOK);
 
     // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
     // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -73,11 +79,9 @@ const SearchBooks = () => {
         }
 
         try {
-            const response = await saveBook(bookToSave, token);
-
-            if (!response.ok) {
-                throw new Error('something went wrong!');
-            }
+            const { data } = await saveBook({
+                variables: { input: bookToSave },
+            });
 
             // if book successfully saves to user's account, save book id to state
             setSavedBookIds([...savedBookIds, bookToSave.bookId]);
