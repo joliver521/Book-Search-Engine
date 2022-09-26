665b465c2872ef15b32d4e96a4f6053f60a3ae40
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-25 22:20:38 -0400

    replaced @apollo/react-hook with @apollo/client in components folder and pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
 
 const client = new ApolloClient({
-    request: (operation) => {
-        const token = localStorage.getItem('id_token');
-
-        operation.setContext({
-            headers: {
-                authorization: token ? `Bearer ${token}` : '',
-            },
-        });
-    },
-    uri: '/graphql',
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
 });
 
 function App() {
@@ -25,15 +37,14 @@ function App() {
             <Router>
                 <>
                     <Navbar />
-                    <Switch>
-                        <Route exact path="/" component={SearchBooks} />
-                        <Route exact path="/saved" component={SavedBooks} />
+                    <Routes>
+                        <Route path="/" element={<SearchBooks />} />
+                        <Route path="/saved" element={<SavedBooks />} />
                         <Route
-                            render={() => (
-                                <h1 className="display-2">Wrong page!</h1>
-                            )}
+                            path="*"
+                            element={<h1 className="display-2">Wrong page!</h1>}
                         />
-                    </Switch>
+                    </Routes>
                 </>
             </Router>
         </ApolloProvider>
