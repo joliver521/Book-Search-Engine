46553f21d8882027125cb514a6b62fb3b56004e4
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-25 22:13:32 -0400

    updated client package.json and server auth.js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,15 +5,9 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    signToken: function ({ username, email, _id }) {
-        const payload = { usernmae, email, _id };
-
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
-
     // function for our authenticated routes
-    authMiddleware: function (req, res, next) {
-        // allows token to be sent via req.body, req.query, or headers
+    authMiddleware: function ({ req }) {
+        // allows token to be sent via  req.query or headers
         let token =
             req.body.token || req.query.token || req.headers.authorization;
 
@@ -22,7 +16,6 @@ module.exports = {
             token = token.split(' ').pop().trim();
         }
 
-        // If no token exists return the request object as it is
         if (!token) {
             return req;
         }
@@ -33,10 +26,15 @@ module.exports = {
             req.user = data;
         } catch {
             console.log('Invalid token');
-            return res.status(400).json({ message: 'invalid token!' });
         }
 
-        // returning up to date request object
+        // return the request object so it can be passed to the resolver as `context`
         return req;
+    },
+
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     },
 };
