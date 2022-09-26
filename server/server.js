eda2af4ee6473a763435df22cc45fa59f9c3d7b0
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-26 09:39:32 -0400

    downloaded starter code to refactor code. trying to get error codes cleaned up. a lot of refactoring

--- server/server.js
+++ server/server.js
@@ -2,26 +2,18 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-
-// importing typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// creating Apollo server and passing in our schema data
-async function startServer() {
-    server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        context: authMiddleware,
-    });
+const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    context: authMiddleware,
+});
 
-    await ApolloServer.start();
-    server.applyMiddleware({ app });
-}
-startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -30,16 +22,24 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
-    app.listen(PORT, () =>
-        console.log(`🌍 Now listening on localhost:${PORT}`)
-    );
-});
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
 
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ' + err);
-});
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(
+                `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+            );
+        });
+    });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
